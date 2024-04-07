b0470b66d602f5bd063f1fd32b5f49f0b30eefb3
Oscar Silva <oscarsilva2356@gmail.com>
2024-04-07 07:45:28 -0700

    Pratice with next

--- src/app/components/ListingCard/index.jsx
+++ src/app/components/ListingCard/index.jsx
@@ -3,7 +3,10 @@ import styles from "./style.module.css";
 const ListingCard = () => {
   return (
     <div className={styles.card}>
-      
+      <img className={styles.card_img} src="./assets/item2.jpg"></img>
+      <p className={styles.card_title}>Stolen Treasure</p>
+      <p className={styles.card_price}>$100</p>
+      <button className={styles.card_button}>Claim Body</button>
     </div>
   );
 };
